34335587500fda390fa3c31aa8c715432e132c51
Thomas Wong <thomaswong@4d.com.hk>
2016-02-17 17:46:20 +0800

    - updated css and add button on home

--- admin/routes/views/home.js
+++ admin/routes/views/home.js
@@ -1,10 +1,16 @@
 var keystone = require('../../../');
+var CMS = require(global.__base + "/enum/CMS");
+var Utility = require(global.__base + '/helper/Utility');
 
 exports = module.exports = function(req, res) {
+
+	var cmsOptionStr = Utility.enumToKeystonOptionStr(CMS);
+	var cmsOption = cmsOptionStr.split(",");
 
 	keystone.render(req, res, 'home', {
 		section: 'home',
 		page: 'home',
+		cmsOption : cmsOption,
 		title: keystone.get('name') || 'Keystone',
 		orphanedLists: keystone.getOrphanedLists()
 	});
