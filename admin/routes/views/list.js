38ee373f668b995893c6505b810ce288b726318b
Thomas Wong <thomaswong@4d.com.hk>
2016-04-14 11:41:51 +0800

    - updated permission log

--- admin/routes/views/list.js
+++ admin/routes/views/list.js
@@ -72,7 +72,7 @@ exports = module.exports = function(req, res) {
 		      }else{
 		        var result = result._doc;
 		        var currentPermission = result.adminPermissions[req.list.path];
-		        console.log("currentPermission["+req.list.path+"]:" + currentPermission);
+		        console.log("User("+req.user._id+") access "+req.list.path + ":" + currentPermission);
 
 		      	if(currentPermission == 'editable'){
 		      		//do nothing
